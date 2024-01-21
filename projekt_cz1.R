5bbd17d01d10d81bfbedfa7023a654206befcbb2
kaamii13 <155846446+kaamii13@users.noreply.github.com>
2024-01-21 14:22:30 +0100

    Zmiana oznaczeń w kolumnach Dochod_kat, Wnioskowana_kw_kat

--- projekt_cz1.R
+++ projekt_cz1.R
@@ -89,7 +89,7 @@ kwantyle <- quantile(dane$Dochod, probs = seq(0.2, 1, by = 0.2))
 przedzialy_kwotowe <- c(-1, 2800, 3800, 4700, 9800, 20000, Inf) #przybliżam do pełnych, "ładnych" kwot :)
 
 # Tworzenie kategorii dochodu na podstawie przedziałów
-X <- cut(dane$Dochod, przedzialy_kwotowe, labels = c(1, 2, 3, 4, 5, 6))
+X <- cut(dane$Dochod, przedzialy_kwotowe, labels = c("[0 ; 2800)", "[2800 ; 3800)", "[3800 ; 4700)", "[4700 ; 9800)", "[9800 ; 20000)", "20000+")
 
 # Stopnie dochodu: 1 - "0-2800", 2 - "2800-3800", 3 - "3800-4700", 4 - "4700-9800", 5 - "9800-20000", 6 - "20000+"
 
@@ -111,7 +111,7 @@ median(dane$WNIOSKOWANA_KWOTA)
 kwantyle <- quantile(dane$WNIOSKOWANA_KWOTA, probs = seq(0.2, 1, by = 0.2))
 przedzialy_kwotowe <- c(-1, kwantyle[-length(kwantyle)], Inf) 
 
-X <- cut(dane$WNIOSKOWANA_KWOTA, przedzialy_kwotowe, labels = c(1, 2, 3, 4, 5))
+X <- cut(dane$WNIOSKOWANA_KWOTA, przedzialy_kwotowe, labels = c("[0 ; 3000)", "[3000 ; 5000)", "[5000 ; 10100)", "[10100 ; 26000)", "26000+"))
 
 # Stopnie: 1 - "0-3000", 2 - "3000-5000", 3 - "5000-10100", 4 - "10100-26000", 5 - "26000+
 
